417a7cdb99c69a7f91ec755f3cac2e3c6c263ba4
Diou Medeiros <diou.182@gmail.com>
2020-06-05 23:51:39 -0300

    feat:  Intialized routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
+
+app.post("/", (request, response) => {
+  return response.json({});
+});
 
 app.listen(3333);
