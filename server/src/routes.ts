469c0b369304b44acdef3dc3eee720b7ea8a1871
Diou Medeiros <diou.182@gmail.com>
2020-06-18 23:48:20 -0300

    feat:Created routes item and points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,56 @@
-import express from "express";
+import express from 'express';
+import knex from './database/connection';
+import Knex from 'knex';
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({});
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.name,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(items);
+});
+
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const ids = await Knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    };
+  });
+
+  await knex('point_items').insert(pointItems);
+
+  return response.json({ success: true });
 });
 
 export default routes;
