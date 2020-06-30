a7184bbd2236082a2d969e246fb17cc6b96523dc
Diou Medeiros <diou.182@gmail.com>
2020-06-29 23:50:50 -0300

    feat: Created new routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,56 +1,16 @@
 import express from 'express';
-import knex from './database/connection';
-import Knex from 'knex';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
+routes.get('/items', itemsController.index);
 
-  const serializedItems = items.map((item) => {
-    return {
-      id: item.id,
-      title: item.name,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-
-  return response.json(items);
-});
-
-routes.post('/points', async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = request.body;
-
-  const ids = await Knex('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id: ids[0],
-    };
-  });
-
-  await knex('point_items').insert(pointItems);
-
-  return response.json({ success: true });
-});
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
